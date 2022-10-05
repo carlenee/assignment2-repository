816f3983e0c7e2b34b8a2a9468c12c0fab06aa8d
carlenee <carleneannabel@gmail.com>
2022-10-06 01:15:12 +0700

    tugas 5

--- Tailwind/Tailwind/urls.py
+++ Tailwind/Tailwind/urls.py
@@ -1,4 +1,4 @@
-"""project_django URL Configuration
+"""Tailwind URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -14,15 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('example_app.urls')),
-    path('katalog/', include('katalog.urls')),
-    path('mywatchlist/', include('mywatchlist.urls')),
-    path('todolist/',include('todolist.urls')),
-    path("__reload__/", include("django_browser_reload.urls")),
 ]
-
-# TODO: Implement Routings Here
